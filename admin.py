85f4272e972ce2f61ea71ac2393130e0123a711e
Nivashinik <72958365+Nivashinik@users.noreply.github.com>
2021-06-15 08:43:40 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from my_app.models import formcontact
-from my_app.models import jobcontact
+#from my_app.models import formcontact
+#from my_app.models import jobcontact
 from my_app.models import Contact
 
 # Register your models here.
-admin.site.register(formcontact)
-admin.site.register(jobcontact)
+#admin.site.register(formcontact)
+#admin.site.register(jobcontact)
 admin.site.register(Contact)
